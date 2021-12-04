f1730cd9562aaa39e75f62b515a6dade01c4669f
EmilHarutyunyan <emil.h098@gmail.com>
2021-12-04 16:41:41 +0400

    basic-example

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./style.css";
 
-const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
+  <React.StrictMode>
+    {/* BrowserRouter պետքա լինի գլխավոր ծնողը մեր հավելվածի(app-ի) և այն թույլ է տալի անել routing մեր app-ի մեջ*/}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
